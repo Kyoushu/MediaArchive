ac382a71133beefd5cc0a0ea41f7a619dc7eef09
Kyoushu <kyoushu@gmail.com>
2014-05-27 20:32:22 +0100

    Refactored SCSS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Kyoushu\MediaBundle\KyoushuMediaBundle(),
             new Kyoushu\FoundationBundle\KyoushuFoundationBundle($this),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Kyoushu/MediaBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kyoushu\MediaBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="User")
+ */
+class User extends BaseUser{
+    
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    public function __construct(){
+        parent::__construct();
+        $this->roles = array();
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+    
+}
